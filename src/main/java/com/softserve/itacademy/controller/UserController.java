6c35c41537fa6c61f2f0c26acdf74692bc604333
DavidBabiy <43074459+DavidBabiy@users.noreply.github.com>
2020-12-22 22:00:21 +0200

    add frontend template

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -2,7 +2,6 @@ package com.softserve.itacademy.controller;
 
 import com.softserve.itacademy.entity.dto.UserDto;
 import com.softserve.itacademy.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/home")
-public class HomeController {
+@RequestMapping("/api/users")
+public class UserController {
 
     private final UserService userService;
 
-    public HomeController(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public ResponseEntity<List<UserDto>> findAll() {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
