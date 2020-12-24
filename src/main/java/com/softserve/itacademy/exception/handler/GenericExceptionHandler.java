7e79f31bcf8d7c043044a711088abf7d6661d366
David <david.babiy1995@gmail.com>
2020-12-24 02:44:54 +0200

    add advice controller

---
+++ src/main/java/com/softserve/itacademy/exception/handler/GenericExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.softserve.itacademy.exception.handler;
+
+import com.softserve.itacademy.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
+@Slf4j
+@ControllerAdvice
+public class GenericExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException exception) {
+        log.debug(exception.getMessage());
+        return new ResponseEntity<>(exception.getMessage(), FORBIDDEN);
+    }
+
+}
