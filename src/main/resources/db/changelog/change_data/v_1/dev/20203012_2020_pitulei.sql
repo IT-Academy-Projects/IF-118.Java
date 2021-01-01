8fc98877ed3fc9f2995b9bb66ff006279195030c
Andrii Pitulei <andriypituley@gmail.com>
2021-01-01 23:22:48 +0200

    Quick encoding fix

--- src/main/resources/db/changelog/change_data/v_1/dev/20203012_2020_pitulei.sql
+++ src/main/resources/db/changelog/change_data/v_1/dev/20203012_2020_pitulei.sql
@@ -4,15 +4,15 @@
 
 -- USERS
 INSERT INTO users(id, name, password, email, created_at, updated_at)
-VALUES (3, 'Admin', '$2a$08$GtMUESLtMtdrXXMnlFNhJeafzGdXCqUXGM', 'admin@example.com', '2020-12-23 13:40:09',
+VALUES (3, 'Admin', '$2a$10$TWmwkXgjHAUrhy0XAAu/6ejyl.dPhu7EnUZK4Dteo/HbXztd4Htdi', 'admin@example.com', '2020-12-23 13:40:09',
         '2020-12-23 13:40:09');
 
 INSERT INTO users(id, name, password, email, created_at, updated_at)
-VALUES (2, 'Student', '$2a$08$GtMUESLtMtdrXXMnlFNhJeafzGdXCqUXGM', 'student@example.com', '2020-12-23 13:40:09',
+VALUES (2, 'Student', '$2a$10$TWmwkXgjHAUrhy0XAAu/6ejyl.dPhu7EnUZK4Dteo/HbXztd4Htdi', 'student@example.com', '2020-12-23 13:40:09',
         '2020-12-23 13:40:09');
 
 INSERT INTO users(id, name, password, email, created_at, updated_at)
-VALUES (1, 'Teacher', '$2a$08$GtMUESLtMtdrXXMnlFNhJeafzGdXCqUXGM', 'teacher@example.com', '2020-12-23 12:40:09',
+VALUES (1, 'Teacher', '$2a$10$TWmwkXgjHAUrhy0XAAu/6ejyl.dPhu7EnUZK4Dteo/HbXztd4Htdi', 'teacher@example.com', '2020-12-23 12:40:09',
         '2020-12-23 12:40:09');
 
 -- USERS_ROLES
