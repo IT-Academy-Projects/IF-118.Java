f6cfac06530054f2918cc032e22452d40e052298
Julia Martyn <juliamartyn@users.noreply.github.com>
2021-01-06 21:48:57 +0200

    Fix GroupController

--- src/main/resources/static/js/create-group.js
+++ src/main/resources/static/js/create-group.js
@@ -2,7 +2,7 @@ function createGroup() {
     let groupName = {name: $('#name').val()};
     $.ajax({
         type: "POST",
-        url: `/api/v1/groups/create/users/${localStorage.getItem("owner_id")}`,
+        url: `/api/v1/groups}`,
         data: JSON.stringify(groupName),
         contentType: "application/json; charset=utf-8",
         success: function() {
