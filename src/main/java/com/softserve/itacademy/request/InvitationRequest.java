7e1914c302670f88a5694a234590db34cab1aa31
Julia Martyn <52349144+juliamartyn@users.noreply.github.com>
2021-01-08 21:51:37 +0200

    Added invitation request/response

---
+++ src/main/java/com/softserve/itacademy/request/InvitationRequest.java
@@ -0,0 +1,23 @@
+package com.softserve.itacademy.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvitationRequest {
+
+    private LocalDateTime expirationDate;
+    private Boolean approved;
+    private String link;
+    private int userId;
+    private int courseId;
+    private int groupId;
+
+}
