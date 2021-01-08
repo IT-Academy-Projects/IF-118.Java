edd9ff09e013e7b2a9d48ee3d4d5c63b52a9aee4
Julia Martyn <52349144+juliamartyn@users.noreply.github.com>
2021-01-08 17:15:07 +0200

    Update create-group.js

--- src/main/resources/static/js/create-group.js
+++ src/main/resources/static/js/create-group.js
@@ -2,7 +2,7 @@ function createGroup() {
     let groupName = {name: $('#name').val()};
     $.ajax({
         type: "POST",
-        url: `/api/v1/groups}`,
+        url: `/api/v1/groups`,
         data: JSON.stringify(groupName),
         contentType: "application/json; charset=utf-8",
         success: function() {
