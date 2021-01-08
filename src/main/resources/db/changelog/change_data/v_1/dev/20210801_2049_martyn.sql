48233f2fdca2d54b48329c326721cf1201457a8d
Julia Martyn <juliamartyn@users.noreply.github.com>
2021-01-08 21:04:19 +0200

    Added script for create invitation table

---
+++ src/main/resources/db/changelog/change_data/v_1/dev/20210801_2049_martyn.sql
@@ -0,0 +1,16 @@
+-- liquibase formatted sql
+-- changeSet Martyn:change_data-1.0.0 endDelimiter:; splitStatements:true
+
+CREATE TABLE IF NOT EXISTS `invitation` (
+    `id` INT NOT NULL,
+    `expiration_date` DATETIME NOT NULL,
+    `approved` BOOLEAN DEFAULT FALSE ,
+    `link` VARCHAR(255) NOT NULL,
+    `user_id` INT NOT NULL,
+    `course_id` INT DEFAULT NULL,
+    `group_id` INT DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_user_invitation` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    CONSTRAINT `fk_course_invitation` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+    CONSTRAINT `fk_group_invitation` FOREIGN KEY (`group_id`) REFERENCES `student_groups` (`id`)
+);
