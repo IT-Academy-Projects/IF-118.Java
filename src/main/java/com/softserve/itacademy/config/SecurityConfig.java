dc95248faf6443cf7b21253e29a94396279a1b27
MykhailoIvanyshyn <mykhailoivanyshyn@gmail.com>
2021-01-10 14:25:05 +0200

    Small fix

--- src/main/java/com/softserve/itacademy/config/SecurityConfig.java
+++ src/main/java/com/softserve/itacademy/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .logoutSuccessUrl("/")
                         .permitAll()
                 .and()
-                    .rememberMe()
+                    .rememberMe();
     }
 
     @Override
