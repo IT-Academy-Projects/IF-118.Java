714da1c8909286ceb01af4eee58c00e883903e88
Julia Martyn <juliamartyn@users.noreply.github.com>
2021-01-13 21:27:50 +0200

    Added invitation creating forms in courses and group

--- src/main/resources/static/js/invitation.js
+++ src/main/resources/static/js/invitation.js
@@ -1,14 +1,35 @@
-function onSubmit() {
+function inviteToCourse() {
+  let course_id = parseInt(window.location.toString().split("id=")[1])
   let body = {
     email: document.getElementById("email").value,
-    courseId: parseInt(document.getElementById("courseId").value, 10),
-    groupId: parseInt(document.getElementById("groupId").value, 10),
+    courseId: course_id
   };
-  fetch("http://localhost:8080/api/v1/invitation", {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-    },
+  $.ajax({
+    type: "POST",
+    url: `/api/v1/invitation`,
+    data: JSON.stringify(body),
+    contentType: "application/json; charset=utf-8",
+    success: function () {
+      getCourse(course_id);
+      $("#close").click();
+    }
+  });
+}
+
+function inviteToGroup() {
+  let group_id = parseInt(window.location.toString().split("id=")[1])
+  let body = {
+    email: document.getElementById("email").value,
+    groupId: group_id
+  };
+  $.ajax({
+    type: "POST",
+    url: `/api/v1/invitation`,
+    data: JSON.stringify(body),
+    contentType: "application/json; charset=utf-8",
+    success: function () {
+      showGroupInfo(group_id);
+      $("#close").click();
+    }
   });
 }
