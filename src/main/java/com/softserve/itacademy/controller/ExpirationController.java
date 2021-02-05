7909dd1c8123f100a18fbeecbb2ba2ce541b9478
Voldmare <avolwo@gmail.com>
2021-02-05 17:28:16 +0200

    Resolver SonarCloud code smells.

--- src/main/java/com/softserve/itacademy/controller/ExpirationController.java
+++ src/main/java/com/softserve/itacademy/controller/ExpirationController.java
@@ -5,7 +5,6 @@ import com.softserve.itacademy.response.MaterialExpirationResponse;
 import com.softserve.itacademy.service.MaterialExpirationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/softserve/itacademy/entity/MaterialExpiration.java
+++ src/main/java/com/softserve/itacademy/entity/MaterialExpiration.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Getter
