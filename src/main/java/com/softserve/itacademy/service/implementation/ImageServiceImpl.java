9d8a4e9d6f25ec25f95218290ae56404f21ab360
David <david.babiy1995@gmail.com>
2021-02-11 18:12:55 +0200

    add tests for ImageServiceImpl fixed image compressing

--- src/main/java/com/softserve/itacademy/service/implementation/ImageServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/implementation/ImageServiceImpl.java
@@ -4,6 +4,7 @@ import com.softserve.itacademy.exception.FileProcessingException;
 import com.softserve.itacademy.exception.NotFoundException;
 import com.softserve.itacademy.repository.ImageRepository;
 import com.softserve.itacademy.service.ImageService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,29 +27,30 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public byte[] findImageById(Integer id) {
-        if (!imageRepository.existsById(id)){
+        if (!imageRepository.existsById(id)) {
             throw new NotFoundException("Resource not found");
         }
         return imageRepository.findFileById(id);
     }
 
     public byte[] compress(MultipartFile file) {
-        try {
 
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(file.getBytes());
+        if (!file.getContentType().equals(MediaType.IMAGE_JPEG_VALUE) &&
+            !file.getContentType().equals(MediaType.IMAGE_PNG_VALUE)) {
+            throw new FileProcessingException("Not valid media type. Should receive JPG or PNG");
+        }
+
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             ByteArrayInputStream inputStream = new ByteArrayInputStream(file.getBytes())) {
+
             BufferedImage originalImage = ImageIO.read(inputStream);
-            inputStream.close();
-
             java.awt.Image resultingImage = originalImage.getScaledInstance(TARGET_IMAGE_SIZE, TARGET_IMAGE_SIZE, java.awt.Image.SCALE_SMOOTH);
             BufferedImage outputImage = new BufferedImage(TARGET_IMAGE_SIZE, TARGET_IMAGE_SIZE, BufferedImage.TYPE_INT_RGB);
             outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
 
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             ImageIO.write(outputImage, "png", byteArrayOutputStream);
-            byte[] imageByteArray = byteArrayOutputStream.toByteArray();
-            byteArrayOutputStream.close();
 
-            return imageByteArray;
+            return byteArrayOutputStream.toByteArray();
         } catch (IOException e) {
             throw new FileProcessingException("Cannot compress image");
         }
