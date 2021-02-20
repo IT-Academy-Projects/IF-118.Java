f96a0cb1e3fdd7847fff37fb42308fa1a435cfab
Andrii Pitulei <andriypituley@gmail.com>
2021-02-20 16:25:10 +0200

    Configuration for integration tests

---
+++ src/test/java/com/softserve/itacademy/tools/HttpUtil.java
@@ -0,0 +1,29 @@
+package com.softserve.itacademy.tools;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpUtil {
+
+    public static <T> T retrieveResourceFromResponse(HttpResponse response, Class<T> clazz)
+            throws IOException {
+
+        String jsonFromResponse = EntityUtils.toString(response.getEntity());
+        System.out.println(jsonFromResponse);
+        ObjectMapper mapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper.readValue(jsonFromResponse, clazz);
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
